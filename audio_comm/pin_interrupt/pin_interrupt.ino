e3556657d7b80a2391a6ea5431ed75d3e6fbd83a
marcb23 <marc.bucchieri@tufts.edu>
2013-06-14 14:58:52 -0400

    last commit before implementing parsing code

--- audio_comm/pin_interrupt/pin_interrupt.ino
+++ audio_comm/pin_interrupt/pin_interrupt.ino
@@ -11,7 +11,6 @@ volatile byte index;
 int ref_index;
 volatile byte byte_index;
 volatile byte storage_arr[ARR_SIZE];
-volatile long spacing;
 
 void setup() {
   Serial.begin(9600);
@@ -25,16 +24,15 @@ void setup() {
   for(int i=0;i<ARR_SIZE;i++) {
     storage_arr[i] = 0;
   }
-  spacing = 0;
   changes = 0;
 
   cli();
-//set timer2 interrupt at 1kHz
+//set timer2 interrupt at 1.6kHz
   TCCR2A = 0;// set entire TCCR0A register to 0
   TCCR2B = 0;// same for TCCR0B
   TCNT2  = 0;//initialize counter value to 0
-  // set compare match register for 1000hz increments
-  OCR2A = 250;// = (16*10^6) / (1000*64) - 1 (must be <256)
+  // set compare match register for 1600hz increments
+  OCR2A = 156;// = (16*10^6) / (1600*64) - 1 (must be <256)
   // turn on CTC mode
   TCCR2A |= (1 << WGM21);
   // Set prescaler
@@ -48,11 +46,11 @@ void setup() {
   TIMSK2 |= (1 << OCIE2A);
   
   
-//  // setup for the input capture interrupt
-//  TCCR1A = _BV(WGM10) | _BV(WGM11);             // Timer 1 is Phase-correct 10-bit PWM. 
-//  TCCR1B = _BV(WGM13) | _BV(WGM12) | _BV(CS11); // div 8 clock prescaler to give .5us ticks on 16mhz clock
-//  TCCR1B |= _BV(ICES1);                         // enable input capture
-//  TIMSK1 =  _BV(ICIE1);                         // enable input capture interrupt for timer 1
+  // setup for the input capture interrupt
+  TCCR1A = _BV(WGM10) | _BV(WGM11);             // Timer 1 is Phase-correct 10-bit PWM. 
+  TCCR1B = _BV(WGM13) | _BV(WGM12) | _BV(CS11); // div 8 clock prescaler to give .5us ticks on 16mhz clock
+  TCCR1B |= _BV(ICES1);                         // enable input capture
+  TIMSK1 =  _BV(ICIE1);                         // enable input capture interrupt for timer 1
 
 sei();
 }
@@ -102,7 +100,7 @@ void loop() {
 ISR(TIMER2_COMPA_vect) {
 // if one of the last four pulses was true or current transmission is true 
 //  if(!(byte_index > 6 && storage_arr[index] == 0) || (PINB & B0000001)){  
-  if((PINB & B0000001) || num_zeros < 250) {
+  if((PINB & B0000001) || num_zeros < 120) {
     storage_arr[index] = (storage_arr[index] << 1) | (PINB & B0000001);
     // increment byte_index and make sure its less than 8
     if(PINB & B0000001)
@@ -116,19 +114,20 @@ ISR(TIMER2_COMPA_vect) {
   }
 }
 
-//ISR(TIMER1_CAPT_vect) {
-//   if(bit_is_set(TCCR1B ,ICES1)){  // was rising edge detected? - last bit was 0  
-//      storage_arr[index]  = (storage_arr[index] << 1);
-//   } else {                         // falling edge was detected 
-//      storage_arr[index]  = (storage_arr[index] << 1) | 1;
-//   }     
-//   TCCR1B ^= _BV(ICES1);                 // toggle bit value to trigger on the other edge
-////   capture_changes++;
-//   changes++;
-//   
-//    // increment byte_index and make sure its <= 8
-//    byte_index++; 
-//    byte_index &= B111; 
-//    if(!byte_index) index++;  //byte_index was just reset so increment the array index
-//   TCNT2  = 0; // reset timer2 counter
-//}
+ISR(TIMER1_CAPT_vect) {
+   if(!bit_is_set(TCCR1B ,ICES1)){  // was falling edge detected? - last bit was 1  
+      storage_arr[index]  = (storage_arr[index] << 1);
+   } else {                         // rising edge was detected 
+      storage_arr[index]  = (storage_arr[index] << 1) | 1;
+   }     
+   TCCR1B ^= _BV(ICES1);                 // toggle bit value to trigger on the other edge
+//   capture_changes++;
+   changes++;
+   num_zeros = 0;
+   
+    // increment byte_index and make sure its <= 8
+    byte_index++; 
+    byte_index &= B111; 
+    if(!byte_index) index++;  //byte_index was just reset so increment the array index
+   TCNT2  = 0; // reset timer2 counter
+}
