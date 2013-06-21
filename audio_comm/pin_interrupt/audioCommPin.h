2e48e227359cb07b2241e7f5efa64d3ac139c4ce
marcb23 <marc.bucchieri@tufts.edu>
2013-06-21 11:22:22 -0400

    all the functions and variables for audio communication are condensed into the .h and .cpp files

--- audio_comm/pin_interrupt/audioCommPin.h
+++ audio_comm/pin_interrupt/audioCommPin.h
@@ -3,10 +3,6 @@
 
 #include "Arduino.h"
 
-const byte HEADER = B100011;
-const byte TAIL = B111111;
-const int ARR_SIZE = 400;
-
 #define SEARCH2 3   
 #define SEARCH3 7
 #define SEARCH4 15
@@ -17,23 +13,18 @@ const int ARR_SIZE = 400;
 
 #define FINDGROUP SEARCH3
 
-
-volatile byte num_zeros;
-volatile int changes;
-int last_state;
-long time_of_last;
-volatile int index;
-volatile byte bit_index;
-
-volatile unsigned short buffer;
-volatile byte storage_arr[ARR_SIZE];
-char char_arr[(ARR_SIZE*2)/5];        // adjust size to account for storage strategy
-int char_index;
-
-volatile unsigned long overflow_1600;
-unsigned long seconds;
-
 void initAudioPin();
+void resetStorage();
+void pinChange();
+int getTime();
+boolean checkTransmission();
+void resetStorage();
+void resetChars();
+void parseArray();
+void findBuffer(int*,byte*,byte);
+char getChar(int, byte);
+int pow(int,int);
+void printArray();
 
 
 
